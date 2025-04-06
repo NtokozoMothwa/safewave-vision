9c78eb260d61912bccd4f5de9bb606e33fd48c2e
ntokozo mothwa <131828819+NtokozoMothwa@users.noreply.github.com>
2025-04-06 19:24:21 +0200

    Create threat.ts

---
+++ api/routes/threat.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { scoreUserRisk } from "../utils/threat";
+
+const router = Router();
+
+router.post("/", (req, res) => {
+  const { heartRate, oxygenLevel, temperature, locationRiskZone } = req.body;
+  const riskScore = scoreUserRisk({ heartRate, oxygenLevel, temperature, locationRiskZone });
+  return res.status(200).json({ riskScore });
+});
+
+export default router;
