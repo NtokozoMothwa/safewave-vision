ea21df464478db8213578bc0dfc26d82883158f8
ntokozo mothwa <131828819+NtokozoMothwa@users.noreply.github.com>
2025-04-07 21:23:22 +0200

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,21 +1,27 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import alertRoutes from "./routes/alert";
 import threatRoutes from "./routes/threat";
+import authRoutes from "./routes/auth";
+import deviceRoutes from "./routes/device";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/device", deviceRoutes);
 app.use("/api/alert", alertRoutes);
 app.use("/api/threat", threatRoutes);
 
 app.get("/", (req, res) => {
-  res.send("SGX SafeSphere API is running");
+  res.send("🌐 SGX SafeSphere Backend Running");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
 });
