1f37c8859e0490f920301bb762ca59edd612240e
ntokozo mothwa <131828819+NtokozoMothwa@users.noreply.github.com>
2025-04-08 00:02:00 +0200

    Create App.tsx

---
+++ api/src/App.tsx
@@ -0,0 +1,11 @@
+import LiveAlerts from "./components/LiveAlerts";
+
+function App() {
+  return (
+    <main className="min-h-screen bg-gray-50 p-8">
+      <LiveAlerts />
+    </main>
+  );
+}
+
+export default App;
