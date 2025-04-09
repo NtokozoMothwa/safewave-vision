5aef556c7da2d726750850fdecbbcb3dafb9e673
ntokozo mothwa <131828819+NtokozoMothwa@users.noreply.github.com>
2025-04-09 05:34:34 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import './styles/global.css';
 import "./index.css";
 import { socket } from "./lib/socket";
 
