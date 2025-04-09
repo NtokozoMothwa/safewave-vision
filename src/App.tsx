183cfda31d29a68820ccd94120b1dfb1ad6d9da9
ntokozo mothwa <131828819+NtokozoMothwa@users.noreply.github.com>
2025-04-09 05:30:05 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import AlertFeed from "./components/AlertFeed";
 import React from "react";
 import LiveAlert from "./components/LiveAlert";
-
+<AlertFeed />
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900">
       <LiveAlert />
       <main className="p-4">
         <h1 className="text-2xl font-bold">SafeWave Vision</h1>
+        
         <p className="mt-2">Welcome to SGX's safety platform 🚨</p>
       </main>
     </div>
