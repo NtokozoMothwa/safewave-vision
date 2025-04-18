fa6e8a3d80d6343c161d411bde19b1c94dc88fc1
ntokozo mothwa <131828819+NtokozoMothwa@users.noreply.github.com>
2025-04-18 19:39:27 +0200

    Create ResponderNav.tsx

---
+++ src/components/nav/ResponderNav.tsx
@@ -0,0 +1,11 @@
+const ResponderNav = () => (
+  <nav className="bg-blue-900 text-white p-4">
+    <ul className="flex space-x-6">
+      <li>Incidents</li>
+      <li>Map</li>
+      <li>Comms</li>
+    </ul>
+  </nav>
+);
+
+export default ResponderNav;
