c9200bc85901aac09e5f3bd2d2942d534ce78bc1
ntokozo mothwa <131828819+NtokozoMothwa@users.noreply.github.com>
2025-04-18 19:38:49 +0200

    Create AdminNav.tsx

---
+++ src/components/nav/AdminNav.tsx
@@ -0,0 +1,12 @@
+const AdminNav = () => (
+  <nav className="bg-gray-900 text-white p-4">
+    <ul className="flex space-x-6">
+      <li>Users</li>
+      <li>Analytics</li>
+      <li>Settings</li>
+      <li>Integrations</li>
+    </ul>
+  </nav>
+);
+
+export default AdminNav;
