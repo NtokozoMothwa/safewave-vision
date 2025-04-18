1cdcf88a06c50a9c38a6908b337394d1e50e9e5f
ntokozo mothwa <131828819+NtokozoMothwa@users.noreply.github.com>
2025-04-18 19:40:05 +0200

    Create GuardNav.tsx

---
+++ src/components/nav/GuardNav.tsx
@@ -0,0 +1,11 @@
+const GuardNav = () => (
+  <nav className="bg-green-900 text-white p-4">
+    <ul className="flex space-x-6">
+      <li>My Zone</li>
+      <li>Alerts</li>
+      <li>Reports</li>
+    </ul>
+  </nav>
+);
+
+export default GuardNav;
