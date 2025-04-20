cbe415c946726e7b01ed021b13b874919e05eaa5
ntokozo mothwa <131828819+NtokozoMothwa@users.noreply.github.com>
2025-04-20 18:11:20 +0200

    Create map.ts

---
+++ src/utils/map.ts
@@ -0,0 +1,4 @@
+export const jumpToLocation = (lat: number, lng: number) => {
+  alert(`Jumping to location:\nLatitude: ${lat}\nLongitude: ${lng}`);
+  // In the future: center the map on these coordinates
+};
